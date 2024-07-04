af430c2279c4b4e22725e38e3eefafddb349456e
ns_gbljh <matveyerohin228@gmail.com>
2024-07-04 19:54:34 +0400

    power management

--- configuration.nix
+++ configuration.nix
@@ -140,4 +140,7 @@
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 	system.stateVersion = "24.05"; # Did you read the comment?
+
+	powerManagement.enable = true;
+	services.tlp.enable = true;
 }
